0d9c965df89e61037edd05ef02202d7aabd581fc
MattOrvin <matthew.orvin@gmail.com>
2019-09-26 10:34:28 -0500

    save feature working; needs user assoc

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,17 @@
 class EventsController < ApplicationController
-    # before_action :find_event, only: [:create]
-
+    
     def index
         @events = Event.all
     end
 
     def create
-        # @event = Event.find(params[:eventbrite_id]) || 0
-        # if !@event
-            @event = Event.new(event_params)
-            @event.save
-        # end
+        @event = Event.find_or_create_by(event_params)
         render json: {event: @event}
     end
 
     private
     def event_params
-        params.require(:event).permit(:name, :eventbrite_id, :venue_id)
+        params.require(:event).permit(:name, :eventbrite_id, :venue_id, :id)
     end
 
-    # def find_event
-    #     @current_event = Event.find(params[:eventbrite_id])
-    # end
 end
